75224a620b5d5d6a9bc372285359473d7837f61f
Diego da Costa Braga <diego.cstbraga@gmail.com>
2021-07-02 16:21:58 -0300

    parte 4 - ok

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ snake[0] = {
 
 let direction = 'right'
 
+let food = {
+	x: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function createBackground(){
     context.fillStyle = '#1182b2'
     context.fillRect(0, 0, 16 * box, 16 * box) // parâmetros (x, y, altura, largura)
@@ -21,6 +26,11 @@ function createSnake(){
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 
+}
+
+function drawFood (){
+	context.fillStyle = 'red'
+	context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', updateDirection)
@@ -41,6 +51,7 @@ function startGame(){
 
 	createBackground()
 	createSnake()
+	drawFood()
 
 	let snakeX = snake[0].x
 	let snakeY = snake[0].y
